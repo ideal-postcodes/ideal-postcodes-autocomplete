fcdc6bb6e67c36d62f4826f3f241639c81cba734
Chris Blanchard <cablanchard@gmail.com>
2017-03-21 16:06:58 +0000

    Update browsers

--- test/config/platforms.browserstack.js
+++ test/config/platforms.browserstack.js
@@ -1,134 +1,127 @@
 "use strict";
 
-module.exports = {
-	// OS10
+const platformFilter = (process.env.PLATFORMS || process.env.PLATFORM || "")
+	.split(",")
+	.filter(elem => elem)
+	.map(elem => elem.toLowerCase());
 
-	// OS10.Firefox
-
-	"OSX_FireFox": { 
-		base: "BrowserStack",
-		browser: "firefox",
-		os: "OS X",
-		os_version: "Sierra",
-		browser_version: "47.0"
-	},
-
-	// OS10.Chrome
-
-	"OSX_Chrome": {
-		base: "BrowserStack",
-		browser: "Chrome",
-		browser_version: "52.0",
-		os: "OS X",
-		os_version: "Sierra"
-	},
-
-	// OS10.Safari
-
-	"Safari_6": {
-		base: "BrowserStack",
-		browser: "Safari",
-		os: "OS X",
-		os_version: "Mountain Lion",
-		browser_version: "6.2"
-	},
-	"Safari_7": {
-		base: "BrowserStack",
-		browser: "Safari",
-		os: "OS X",
-		os_version: "Mavericks",
-		browser_version: "7.1"
-	},
-	"Safari_8": {
-		base: "BrowserStack",
-		browser: "Safari",
-		os: "OS X",
-		os_version: "Yosemite",
-		browser_version: "8.0"
-	},
-	"Safari_9": {
-		base: "BrowserStack",
-		browser: "safari",
-		browser_version: "9.0",
-		os: "OS X",
-		os_version: "El Capitan"
-	},
-
-	// Windows
-
-	// Windows.Firefox
-
-	"Windows_FireFox": {
-		base: "BrowserStack",
-		browser: "firefox",
-		os: "Windows",
-		os_version: "10",
-		browser_version: "47.0"
-	},
-
-	"Windows_Chrome": {
-		base: "BrowserStack",
-		browser: "chrome",
-		browser_version: "52.0",
-		os: "Windows",
-		os_version: "10"
-	},
-
-	// Windows.Edge
-	"Edge": {
-		base: "BrowserStack",
-		browser: "Edge",
-		browser_version: "13.0",
-		os: "Windows",
-		os_version: "10"
-	},
-
-	// Windows.IE
-
-	// "InternetExplorer_9": {
+const platforms = {
+	// Requires LIVE=true due to incompatible stubbing lib
+	// "windows.ie.9": {
 	// 	base: "BrowserStack",
 	// 	browser: "IE",
 	// 	browser_version: "9.0",
 	// 	os: "Windows",
 	// 	os_version: "7"
 	// },
-	"InternetExplorer_10": {
+	// "windows.opera.12.16": {
+	// 	base: "BrowserStack",
+	// 	browser: "Opera",
+	// 	os: "Windows",
+	// 	os_version: "7",
+	// 	browser_version: "12.16"
+	// },
+
+	"osx.firefox.47": { 
+		base: "BrowserStack",
+		browser: "firefox",
+		os: "OS X",
+		os_version: "Sierra",
+		browser_version: "47.0"
+	},
+	"osx.chrome.52": {
+		base: "BrowserStack",
+		browser: "Chrome",
+		browser_version: "52.0",
+		os: "OS X",
+		os_version: "Sierra"
+	},
+	"osx.safari.6": {
+		base: "BrowserStack",
+		browser: "Safari",
+		os: "OS X",
+		os_version: "Mountain Lion",
+		browser_version: "6.2"
+	},
+	"osx.safari.7": {
+		base: "BrowserStack",
+		browser: "Safari",
+		os: "OS X",
+		os_version: "Mavericks",
+		browser_version: "7.1"
+	},
+	"osx.safari.8": {
+		base: "BrowserStack",
+		browser: "Safari",
+		os: "OS X",
+		os_version: "Yosemite",
+		browser_version: "8.0"
+	},
+	"osx.safari.9": {
+		base: "BrowserStack",
+		browser: "safari",
+		browser_version: "9.0",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+	"windows.firefox.47": {
+		base: "BrowserStack",
+		browser: "firefox",
+		os: "Windows",
+		os_version: "10",
+		browser_version: "47.0"
+	},
+	"windows.chrome.52": {
+		base: "BrowserStack",
+		browser: "chrome",
+		browser_version: "52.0",
+		os: "Windows",
+		os_version: "10"
+	},
+	"windows.edge.14": {
+		base: "BrowserStack",
+		browser: "Edge",
+		browser_version: "14.0",
+		os: "Windows",
+		os_version: "10"
+	},
+	"windows.edge.13": {
+		base: "BrowserStack",
+		browser: "Edge",
+		browser_version: "13.0",
+		os: "Windows",
+		os_version: "10"
+	},
+	"windows.ie.10": {
 		base: "BrowserStack",
 		browser: "IE",
 		browser_version: "10.0",
 		os: "Windows",
 		os_version: "7"
 	},
-	"InternetExplorer_11": {
+	"windows.ie.11": {
 		base: "BrowserStack",
 		browser: "IE",
 		browser_version: "11.0",
 		os: "Windows",
 		os_version: "7"
 	},
-
-	// Windows.Opera
-
-	"Opera": {
+	"windows.opera.43": {
 		base: "BrowserStack",
 		browser: "Opera",
 		os: "Windows",
-		os_version: "7",
-		browser_version: "12.16"
+		os_version: "10",
+		browser_version: "43"
 	},
-
-	// Android
-
-	nexus_5: {
+	"android.galaxy.4.4": {
 		base: "BrowserStack",
 		browserName: "android",
 		platform: "ANDROID",
 		os: "android",
-		os_version: "5.0",
-		device: "Google Nexus 5"
+		os_version: "4.4",
+		device: "Samsung Galaxy S5"
 	},
-
-	nexus_4: {
+	"android.nexus.4.2": {
 		base: "BrowserStack",
 		browserName: "android",
 		platform: "ANDROID",
@@ -136,8 +129,7 @@ module.exports = {
 		os_version: "4.2",
 		device: "Google Nexus 4"
 	},
-
-	nexus: {
+	"android.nexus.4.0": {
 		base: "BrowserStack",
 		browserName: "android",
 		platform: "ANDROID",
@@ -145,10 +137,7 @@ module.exports = {
 		os_version: "4.0",
 		device: "Google Nexus"
 	},
-
-	// iOS
-
-	iphone_5s: {
+	"ios.iphone.5s.7.0": {
 		base: "BrowserStack",
 		browserName: "iPhone",
 		platform: "MAC",
@@ -156,14 +145,30 @@ module.exports = {
 		os_version: "7.0",
 		device: "iPhone 5S"
 	},
-
-	iphone_6s: {
+	"ios.iphone.6s.9.1": {
 		base: "BrowserStack",
 		browserName: "iPhone",
 		platform: "MAC",
 		os: "ios",
 		os_version: "9.1",
 		device: "iPhone 6S"
-	}
-	
+	}	
 };
+
+const exportedPlatforms = {};
+
+// Match specific combination, OS, or browser type
+platformFilter.forEach(platform => {
+	Object.keys(platforms).forEach(availablePlatform => {
+		const platformConfig = platforms[availablePlatform];
+		const matchesFilter = [
+			availablePlatform.toLowerCase(),
+			(platformConfig.os || "").toLowerCase(),
+			(platformConfig.browserName || "").toLowerCase(),
+			(platformConfig.browser || "").toLowerCase()
+		].some(test => test === platform);
+		if (matchesFilter) exportedPlatforms[availablePlatform] = platformConfig;
+	});
+});
+
+module.exports = Object.keys(exportedPlatforms).length > 0 ? exportedPlatforms : platforms;
